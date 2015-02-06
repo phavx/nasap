35885a61a7ecf6a4ed3cc16c72f0d81c5e5608e3
avx <phavx@users.noreply.github.com>
2015-02-06 03:12:16 +0100

    initial commit

---
+++ TODO.md
@@ -0,0 +1,58 @@
+TODO
+====
+
+This is a list of TODOs for _nasap_.
+
+Only two categories exist:
+- **Yes**: is planned, execution in no particular order
+- **Maybe**: might be done if considered useful, lowest prioriy. If some of
+these things are interesting to you _and_ you can come up with a patch, a quick
+implementation is much more likely.
+
+### Yes
+
+- [ ] handle "/" in titles so they don't end up in a filename and give an error
+- [ ] more and better error handling, both of user's faults and malformed feeds
+- [ ] currently we use the time of retrieval for a post, which isn't ideal. We
+    should pick the time out of the feed item itself, but making sure to have a
+    backup for feeds not providing it.
+- [ ] introduce a config file for common settings
+  - [ ] configurable base folder
+  - [ ] configurable output format
+    - [ ] plain text
+    - [ ] markdown
+  - [ ] configurable naming of files
+
+- [ ] support for a list of feeds getting passed to _nasap_
+  - [ ] feed urls
+    - [ ] making sure https errors are no problem (self-signed certs f.e.)
+  - [ ] override a feed's title
+
+- [ ] speed it all up: right now all items are processed one after the other,
+    which is slow and doesn't make proper use of available bandwith and CPUtime.
+    Feeds should be processed in parallel, either limited by X per domain or
+    X general threads. Question is, do it in Python or make use of tools such as
+    f.e. GNU parallel?
+
+- [ ] speed up the processing by having a system which automatically skips
+    already seen posts. Could rely on the feed items id or it's link, f.e.
+
+- [ ] move all internal links to the bottom
+
+### Maybe
+
+- [ ] create some hooks to automate post-processing
+    - [ ] f.e. automatically symlink
+    - [ ] initiate syncing to some other system
+    - [ ] filter (f.e. kill everything containing _keyword_)
+    - [ ] tag new posts via symlinking
+
+- [ ] support libnotify/notify-send to inform the user about new feed items
+    based on some criteria(f.e. "new posts from your favorite author", "new post
+    containing _keyword_", etc).
+
+- [ ] support enclosures like images, audiofiles, etc
+
+All of the above should be done in a way so that the user can easily write some
+scripts to achieve the task. It shouldn't be core functionality, but it's ok to
+add such tools to the repo for the convenience or inspiration of others.
---
+++ nasap.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+nasap - news as simple as possible
+
+This is a simple tool to fetch full text posts based on a RSS/Atom feed and
+store it as a simple textfile to later view it with a tool of your choice.
+
+git repo: https://github.com/phavx/nasap/
+
+Information about AUTHORS, TODO and LICENSE can be found in the respective file
+"""
+
+# standard libs
+from io import open
+from os import environ, makedirs, path
+from urllib2 import urlopen
+from sys import argv, exit
+from textwrap import TextWrapper
+from time import gmtime, strftime
+
+# external dependencies
+import feedparser
+import html2text
+from readability.readability import Document
+
+# functions
+def error(msg, code):
+    """prints an error message and exits the program with the given code"""
+    print msg
+    exit(code)
+
+def current_date_time():
+    """create current date and time in case the feed item doesn't provide it"""
+    return strftime("%Y-%m-%d", gmtime()), strftime("%H:%M", gmtime())
+
+def check_create_dir(directory):
+    """if the passed directory doesn't exist, try to create it"""
+    if path.exists(directory):
+        if path.isdir(directory):
+            return True
+        else:
+            return False
+    else:
+        makedirs(directory)
+        return True
+
+# for now nasap doesn't have a config file or runtime options, so make changes
+# in this section if needed
+#
+# the basedir where all feed items will be stored
+NEWS_DIR = environ["HOME"] + "/news"
+
+
+# some basic sanity checks
+if not check_create_dir(NEWS_DIR):
+    error("Error: %s exists, but isn't a directory!" % NEWS_DIR, 1)
+
+if len(argv) != 2:
+    exit(1)
+
+# needed later, setup a textwrap instance
+wrap = TextWrapper(width=80, replace_whitespace=False)
+
+
+URL = argv[1]
+REFERRER = "/".join(URL.split("/")[:3])
+FEED = feedparser.parse(URL, referrer=REFERRER)
+
+# if we don't already have a directory for this feed, create it
+FEED_DIR = NEWS_DIR + "/" + FEED.feed.title
+if not check_create_dir(FEED_DIR):
+    error("Error: feed directory %s could not be created." % FEED_DIR, 1)
+
+# loop over the feed's items and process them
+for i in range(0, len(FEED["entries"])):
+    html = Document(urlopen(FEED.entries[i].link).read()).summary()
+
+    # stripping html tags and limiting the width to 80 characters
+    h = html2text.HTML2Text(bodywidth=80)
+    h.ignore_images = True
+    body = h.handle(html)
+    #body = html2text(html, bodywidth=80, ignore_images=True)
+    
+
+    # some feed items have really long titles, so better truncate them
+    filename = FEED.entries[i].title[:59]
+    date, time = current_date_time()
+    posted = "%s-%s" % (date, time)    
+
+    # it's just text, but a nice layout certainly isn't bad, so construct it
+    u_line = u"┏" + 78 * u"━"             + u"┓\n"
+    ufill = 76 - len(FEED.feed.title[:53] + FEED.feed.link)
+    header = u"┃ " + FEED.feed.title[:53] + ufill * " " + FEED.feed.link  + u" ┃\n"
+    m_line = u"┣" +  59 *u"━" + u"┳" + 18* u"━" + u"┫\n"
+    bfill = 74 - (len(FEED.entries[i].title[:53] + " ... " ) + len(posted))
+    subline = u"┃ " + FEED.entries[i].title[:53] + " ... " + bfill * " " + u"┃ " + posted + u" ┃\n"    
+    b_line = u"┗" + 59 * u"━" + u"┻" + 18 * u"━" + u"┛\n\n"
+    title = FEED.entries[i].title + "\n\n"
+    #title = wrap.fill(FEED.entries[i].title) + "\n\n"
+    pre_links = "\n" + 80 * u"━" + "\nLinks:\n[*] " + FEED.entries[i].link
+    
+    product = u_line + header + m_line + subline + b_line + title + body + pre_links
+
+
+    # finally, write out the finished product
+    fh = open(FEED_DIR + "/" + "[" + date +"]-[" + time + "]" + " " + filename, "w")
+    fh.write(product)
+    fh.close()
+    
